662f2f94912236f98ca1d9f09493fac7c0fcfded
neaimt <pas120344@gmail.com>
2025-04-03 16:56:24 +0900

    chore: API 호출 시 사용될 서버 주소 및 endpoint 상수 분리

---
+++ glym-app/src/constants/urls.js
@@ -0,0 +1,10 @@
+export const URLS = {
+  BASE: {
+    MAIN: "",
+    TEST: "http://ec2-15-164-102-179.ap-northeast-2.compute.amazonaws.com:8080",
+  },
+  ENDPOINT: {
+    SIGN_UP: "/signup",
+    LOGIN: "/login",
+  },
+};
