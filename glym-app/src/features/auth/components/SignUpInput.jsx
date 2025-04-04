81aaef9d50c5c12e8d67671bbc64c5fa453cd6e9
neaimt <pas120344@gmail.com>
2025-04-04 17:11:50 +0900

    feat: onBlur시 경고 메세지 추가

--- glym-app/src/features/auth/components/SignUpInput.jsx
+++ glym-app/src/features/auth/components/SignUpInput.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { S } from "../style";
 
 export default function SignUpInput({ value, type, required = true, onChange, showCheckButton = false, buttonValue, onCheck, error='' }) {
+    const [errorMessage, setErrorMessage] = useState('');
+
     return (
         <S.InputRow.Container>
             <S.InputRow.Label>{value}{required && (<span style={{ color: "#FF3F77" }}>*</span>)}</S.InputRow.Label>
             <div>
-                <S.InputRow.Input type={type} onChange={onChange} />
-                <S.InputRow.ErrorMessage>{error}</S.InputRow.ErrorMessage>
+                <S.InputRow.Input type={type} onChange={onChange} onBlur={()=> setErrorMessage(error)}/>
+                <S.InputRow.ErrorMessage>{errorMessage}</S.InputRow.ErrorMessage>
             </div>
             {showCheckButton ?
                 (<S.InputRow.Button onClick={onCheck}>{buttonValue}</S.InputRow.Button>)
