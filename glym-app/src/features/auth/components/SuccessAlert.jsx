1778f5f2a7e6da7ca6f3e83d65a049c92332e27d
neaimt <pas120344@gmail.com>
2025-04-09 21:46:10 +0900

    feat: SuccesAlert 컴포넌트 생성
    - 회원가입 성공시 로그인 페이지로 이동하면서 띄울 alert

---
+++ glym-app/src/features/auth/components/SuccessAlert.jsx
@@ -0,0 +1,8 @@
+export default function SuccessAlert() {
+    return (
+        <div style={{ width: "500px", height: "60px",  margin: "0 auto", padding: "10px", textAlign: "center", borderRadius: "10px", border: "1px solid #FFB9CD", backgroundColor: "#FFFFFF" }}>
+            <h1 style={{ fontSize: "20px", fontWeight: "500", color: "#000000" }}>✨ 회원가입이 완료되었습니다! ✨</h1>
+            <h3 style={{ fontSize: "14px", fontWeight: "400", color: "#929292" }}>로그인하신 후 다양한 서비스를 이용할 수 있어요.</h3>
+        </div>
+    );
+}
