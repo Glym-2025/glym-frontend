fa010c067e17c5433309e56a8b010cffd077283a
neaimt <pas120344@gmail.com>
2025-04-09 22:21:50 +0900

    feat: SignInPage에서 SuccessAlert 타이머 추가
    - 5초동안 보이고 사라짐

--- glym-app/src/pages/SignInPage.jsx
+++ glym-app/src/pages/SignInPage.jsx
@@ -1,17 +1,28 @@
+import { useState, useEffect } from "react";
 import AuthForm from "../features/auth/components/AuthForm";
 import SuccessAlert from "../features/auth/components/SuccessAlert";
 
-export default function SignInPage({ state: { signedUp = false} = {} }) {
+export default function SignInPage({ state: { signedUp = false } = {} }) {
+    const [isSignedUp, setIsSignedUp] = useState(signedUp);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsSignedUp(false);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
             <div style={{ 
-                marginTop: signedUp ? "10px" : "100px",
-                gap: "10px",
+                marginTop: isSignedUp ? "20px" : "100px",
+                gap: "20px",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center"
             }}>
-                {signedUp && <SuccessAlert />}
+                {isSignedUp && <SuccessAlert />}
                 <AuthForm />
             </div>
         </>
