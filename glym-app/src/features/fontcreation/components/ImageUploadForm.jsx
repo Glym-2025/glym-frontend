d87014b8b80b5d58bdce3f6e4119222ac35aba4c
neaimt <pas120344@gmail.com>
2025-04-10 18:50:57 +0900

    style: 컴포넌트에서 styled-components 분리
    - style.js 파일로 이동
    - 객체 S 생성, 각 컴포넌트 마다 묶어줌

--- glym-app/src/features/fontcreation/components/ImageUploadForm.jsx
+++ glym-app/src/features/fontcreation/components/ImageUploadForm.jsx
@@ -1,64 +1,15 @@
-import styled from "styled-components";
+import { S } from "../style";
 import fileLogo from "../../../shared/FILE_LOGO.png";
-import { font } from "../../../styles/font";
-
-const Container = styled.div`
-    width: 800px;
-    height: 800px;
-
-    border-radius: 10px;
-    border: 1px solid #D9D9D9;
-    background-color: #FFFFFF;
-`;
-
-const DropBox = styled.div`
-    width: 100%;
-    height: 72%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    ${font(18, 400, 1.5)}
-    color: #929292;
-`;
-
-const FileSelectButton = styled.button`
-    width: 180px;
-    height: 40px;
-
-    margin-top: 15px;
-
-    border-radius: 10px;
-    border: none;
-
-    ${font(18, 400, 1.5)}
-    color: #FFFFFF;
-    background-color: #94C7FE;
-`;
-
-const ImageGuideBox = styled.div`
-    width: 100%;
-    height: 28%;
-
-    text-align: left;
-    padding: 30px;
-    border-top: 1px solid #D9D9D9;
-    color: #929292;
-
-    ${font(18, 400, 1.2)}
-`;
 
 export default function ImageUploadForm() {
     return (
-        <Container>
-            <DropBox>
+        <S.ImageUploadForm.Container>
+            <S.ImageUploadForm.DropBox>
                 <img src={fileLogo} alt="dropBox" style={{ width: "80px", height: "80px", marginBottom: "30px" }} />
                 여기에 파일을 드롭하세요!
-                <FileSelectButton>파일 선택</FileSelectButton>
-            </DropBox>
-            <ImageGuideBox>
+                <S.ImageUploadForm.SelectButton>파일 선택</S.ImageUploadForm.SelectButton>
+            </S.ImageUploadForm.DropBox>
+            <S.ImageUploadForm.GuideBox>
                 <p style={{ marginBottom: "10px", color: "#6B6B6B" }}>업로드 시 주의할 점</p>
                 <ul style={{ listStyleType: "none", padding: 0 }}>
                     <li>- 배경이 너무 어둡거나 패턴이 있으면 안 됩니다.</li>
@@ -68,7 +19,7 @@ export default function ImageUploadForm() {
                     <li>- 파일 형식은 PNG만 지원합니다.</li>
                     <li>- 압축 파일(.zip)은 업로드할 수 없습니다.</li>
                 </ul>
-            </ImageGuideBox>
-        </Container>
+            </S.ImageUploadForm.GuideBox>
+        </S.ImageUploadForm.Container>
     );
 }
---
+++ glym-app/src/features/fontcreation/style.js
@@ -0,0 +1,145 @@
+import styled from "styled-components";
+import { font } from "../../styles/font";
+
+// ImageUploadForm
+const ImageUploadFormContainer = styled.div`
+    min-width: 800px;
+    width: 800px;
+    height: 800px;
+    box-sizing: border-box;
+
+    border-radius: 10px;
+    border: 1px solid #D9D9D9;
+    background-color: #FFFFFF;
+`;
+
+const DropBox = styled.div`
+    width: 100%;
+    height: 72%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    ${font(18, 400, 1.5)}
+    color: #929292;
+`;
+
+const FileSelectButton = styled.button`
+    width: 180px;
+    height: 40px;
+
+    margin-top: 15px;
+
+    border-radius: 10px;
+    border: none;
+
+    ${font(18, 400, 1.5)}
+    color: #FFFFFF;
+    background-color: #94C7FE;
+`;
+
+const ImageGuideBox = styled.div`
+    width: 100%;
+    height: 28%;
+
+    text-align: left;
+    padding: 30px;
+    border-top: 1px solid #D9D9D9;
+    color: #929292;
+
+    ${font(18, 400, 1.2)}
+`;
+
+// FontInfoForm
+const FontInfoFormContainer = styled.div`
+    min-width: 380px;
+    width: 380px;
+    height: 690px;
+    box-sizing: border-box;
+
+    border: 1px solid #D9D9D9;
+    border-radius: 10px;
+    background-color: #FFFFFF;
+
+    ${font(18, 400, 1.5)}
+    color: #929292;
+`;
+
+const InputBox = styled.div`
+    display: flex;
+    gap: 15px;
+
+    margin: 15px 0 8px 0;
+`;
+
+const Input = styled.input`
+    width: 240px;
+    height: 50px;
+    
+    padding: 15px;
+
+    border: 1px solid #929292;
+    border-radius: 10px;
+
+    ${font(18, 400, 1.5)}
+    color: #929292;
+    &:focus {
+        outline: none;
+        border: 1px solid #F43C71;
+    }
+`;
+
+const CheckButton = styled.button`
+    width: 65px;
+    height: 50px;
+    
+    text-align: center;
+
+    border-radius: 10px;
+    border: 1px solid #F43C71;
+
+    ${font(18, 400, 1.5)}
+    color: #F43C71;
+    background-color: #FFFFFF;
+
+    &:hover {
+        background-color: #F1F1F1;
+    }
+`;
+
+const FontGuideBox = styled.div`
+    width: 100%;
+
+    padding: 30px;
+    text-align: left;
+
+    border-top: 1px solid #D9D9D9;
+
+    ${font(16, 400, 1)}
+`;
+
+const GuideTitle = styled.p`
+    margin-bottom: 15px;
+
+    ${font(18, 400, 1)}
+    color: #6B6B6B;
+`;
+
+export const S = {
+    ImageUploadForm: {
+        Container: ImageUploadFormContainer,
+        DropBox: DropBox,
+        SelectButton: FileSelectButton,
+        GuideBox: ImageGuideBox
+    },
+    FontInfoForm: {
+        Container: FontInfoFormContainer,
+        InputBox: InputBox,
+        Input: Input,
+        CheckButton: CheckButton,
+        GuideBox: FontGuideBox,
+        GuideTitle: GuideTitle
+    }
+}
