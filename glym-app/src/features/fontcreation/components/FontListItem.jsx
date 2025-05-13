e39d2cc54d1eade4215acf672079d9c3fc4c5bd0
neaimt <pas120344@gmail.com>
2025-05-13 19:05:10 +0900

    feat: FontListItem 컴포넌트 생성

---
+++ glym-app/src/features/fontcreation/components/FontListItem.jsx
@@ -0,0 +1,14 @@
+export default function FontListItem() {
+    return (
+        <div>
+            <div>ttf</div>
+            <div>
+                <div>
+                    <h1>LeeBaekByGlym 폰트</h1>
+                    <p>2025년 3월 25일</p>
+                </div>
+                <p>이미지</p>
+            </div>
+        </div>
+    );
+}
