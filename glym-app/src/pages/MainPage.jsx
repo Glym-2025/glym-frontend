ac07541b08654a9837a7bf4bd1fc8b7ad72ce281
neaimt <pas120344@gmail.com>
2025-05-16 16:20:59 +0900

    feat: ProcessStepsSection 컴포넌트 생성

--- glym-app/src/pages/MainPage.jsx
+++ glym-app/src/pages/MainPage.jsx
@@ -1,10 +1,11 @@
-import { IntroSection, ExampleSection } from "../features/Intro";
+import { IntroSection, ExampleSection, ProcessStepsSection } from "../features/Intro";
 
 export default function MainPage() {
     return(
         <div>
             <IntroSection />
             <ExampleSection />
+            <ProcessStepsSection />
         </div>
     );
 }
