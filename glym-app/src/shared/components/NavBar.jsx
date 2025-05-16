bb28f66c263333f5bedc85b48dffd5ef90f030a4
neaimt <pas120344@gmail.com>
2025-05-16 18:29:27 +0900

    feat: 마이페이지 삭제

--- glym-app/src/shared/components/NavBar.jsx
+++ glym-app/src/shared/components/NavBar.jsx
@@ -56,7 +56,6 @@ export default function NavBar() {
                 <S.Menu to="/">서비스 소개</S.Menu>
                 <S.Menu to="/fontcreation">폰트제작</S.Menu>
                 <S.Menu to="/fontlist">이용내역</S.Menu>
-                <S.Menu to="/">마이페이지</S.Menu>
                 {isLoggedIn ? ( 
                     <S.Menu onClick={logout}>로그아웃</S.Menu>
                 ) : (
