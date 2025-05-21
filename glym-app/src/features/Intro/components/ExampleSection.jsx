9a1067665cfec0346a56f9dfb159c422fd0dca27
neaimt <pas120344@gmail.com>
2025-05-21 22:15:47 +0900

    feat: 서비스 소개 예시폰트 이미지 교체

--- glym-app/src/features/Intro/components/ExampleSection.jsx
+++ glym-app/src/features/Intro/components/ExampleSection.jsx
@@ -1,19 +1,30 @@
-import exampleImg from "../../../shared/EXAMPLE.png";
+import exampleImg1 from "../../../shared/ex1.png";
+import exampleImg2 from "../../../shared/ex2.png";
+import exampleImg3 from "../../../shared/ex3.png";
+import exampleImg4 from "../../../shared/ex4.png";
 import { S } from "../style";
-
-const exampleFont = ['LeebaekByGlym', 'Nahyun9ByGlym', 'ParkFeByGlym', 'ShapeAnByGlym'];
 
 export default function ExampleSection() {
     return (
         <S.ExampleSection.Container>
             <S.ExampleSection.Title>글씨에 담긴 당신의 온기를 폰트로 간직하세요.</S.ExampleSection.Title>
             <S.ExampleSection.ExampleBox >
-                {exampleFont.map((item, index)=> (
-                    <S.ExampleSection.Example key={index}>
-                        <S.ExampleSection.FontName>{item}</S.ExampleSection.FontName>
-                        <S.ExampleSection.ExampleImg src={exampleImg} />
+                    <S.ExampleSection.Example>
+                        <S.ExampleSection.FontName>LeebaekByGlym</S.ExampleSection.FontName>
+                        <S.ExampleSection.ExampleImg src={exampleImg1} />
                     </S.ExampleSection.Example>
-                ))}
+                    <S.ExampleSection.Example>
+                        <S.ExampleSection.FontName>Nahyun9ByGlym</S.ExampleSection.FontName>
+                        <S.ExampleSection.ExampleImg src={exampleImg2} />
+                    </S.ExampleSection.Example>
+                    <S.ExampleSection.Example>
+                        <S.ExampleSection.FontName>ParkFeByGlym</S.ExampleSection.FontName>
+                        <S.ExampleSection.ExampleImg src={exampleImg3} />
+                    </S.ExampleSection.Example>
+                    <S.ExampleSection.Example>
+                        <S.ExampleSection.FontName>'ShapeAnByGlym'</S.ExampleSection.FontName>
+                        <S.ExampleSection.ExampleImg src={exampleImg4} />
+                    </S.ExampleSection.Example>
             </S.ExampleSection.ExampleBox>
         </S.ExampleSection.Container>
     );
